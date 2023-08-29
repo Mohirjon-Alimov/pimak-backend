e6b765c0ecaca4a58b8668a2ea5de8599be27ab2
Mohirjon <100334903+Mohirjon-Alimov@users.noreply.github.com>
2023-08-29 11:54:46 +0500

    add some changes

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,10 @@ import path from "path";
 import { AppDataSource } from "./config/ormconfig";
 import routes from "./routes";
 import cors from 'cors'
-import {CheckToken} from "./middlewares/checkToken";
 
 const main = async (): Promise<void> => {
   const app: Application = express();
-  const PORT = process.env.PORT;
+  const PORT = process.env.PORT || 9900;
   try {
     app.use(cors())
     app.use(express.json());
